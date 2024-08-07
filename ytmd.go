8784d5daa28a7fdf03f323cbe96de88bcf04eadc
levindecaro <levin@mydream.com.hk>
2024-08-07 18:21:37 +0100

    initial 2.0 commit

--- ytmd.go
+++ ytmd.go
@@ -7,93 +7,11 @@ import (
 	"net/http"
 )
 
-type TrackData struct {
-	Author string `json:"author"`
-	Album  string `json:"album"`
-	Title  string `json:"title"`
-	Cover  string `json:"cover"`
-}
-
-type PlayerData struct {
-	HasSong      bool    `json:"hasSong"`
-	IsPaused     bool    `json:"isPaused"`
-	StatePercent float64 `json:"StatePercent"`
-	LikeStatus   string  `json:"likeStatus"`
-}
-
-func ytmdGetTrack(address string) (*TrackData, error) {
-
-	path := "/query/track"
-	url := "http://" + address + path
-
-	client := &http.Client{}
-
-	req, err := http.NewRequest("GET", url, nil)
-	if err != nil {
-		fmt.Println("Error creating request:", err)
-		return nil, err
-	}
-
-	resp, err := client.Do(req)
-	if err != nil {
-		fmt.Println("Error sending request:", err)
-		return nil, err
-	}
-	defer resp.Body.Close()
-
-	if resp.StatusCode != http.StatusOK {
-		fmt.Println("Request failed with status:", resp.Status)
-		return nil, err
-	}
-
-	var responseData TrackData
-	err = json.NewDecoder(resp.Body).Decode(&responseData)
-	if err != nil {
-		fmt.Println("Error decoding JSON response:", err)
-		return nil, err
-	}
-
-	return &responseData, err
-}
-
-func ytmdGetPlayer(address string) (*PlayerData, error) {
-
-	path := "/query/player"
-	url := "http://" + address + path
-
-	client := &http.Client{}
-	req, err := http.NewRequest("GET", url, nil)
-	if err != nil {
-		fmt.Println("Error creating request:", err)
-		return nil, err
-	}
-
-	resp, err := client.Do(req)
-	if err != nil {
-		fmt.Println("Error sending request:", err)
-		return nil, err
-	}
-	defer resp.Body.Close()
-
-	if resp.StatusCode != http.StatusOK {
-		fmt.Println("Request failed with status:", resp.Status)
-		return nil, err
-	}
-
-	var responseData PlayerData
-	err = json.NewDecoder(resp.Body).Decode(&responseData)
-	if err != nil {
-		fmt.Println("Error decoding JSON response:", err)
-		return nil, err
-	}
-
-	return &responseData, err
-}
-
 func ytmdCmd(address string, token string, playerCmd string) error {
 	// Define the JSON data you want to send
 	data := map[string]interface{}{
 		"command": playerCmd,
+		"data":    "",
 	}
 
 	// Convert the JSON data to bytes
@@ -102,7 +20,7 @@ func ytmdCmd(address string, token string, playerCmd string) error {
 		return err
 	}
 
-	path := "/query"
+	path := "/api/v1/command"
 	url := "http://" + address + path
 
 	client := &http.Client{}
@@ -110,7 +28,7 @@ func ytmdCmd(address string, token string, playerCmd string) error {
 	if err != nil {
 		return err
 	}
-	req.Header.Set("Authorization", "access_token "+token)
+	req.Header.Set("Authorization", token)
 	req.Header.Set("Content-Type", "application/json")
 
 	resp, err := client.Do(req)
